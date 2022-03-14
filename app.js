ccfd765ec911d12dcab799740557cf81a587b074
alexdwu3 <alexdwu3@gmail.com>
2022-03-14 13:11:45 -0500

    drawing and erasing pieces

--- app.js
+++ app.js
@@ -15,67 +15,81 @@ document.addEventListener('DOMContentLoaded', () => {
     const ScoreDisplay = document.querySelector('#score');
     const StartButton = document.querySelector('#start-button');
 
+    // the following piece configurations have 4 numbers representing the 4 boxes in array "squares"
+    // that we want to style so that they represent the correct tetris piece
     // piece configurations: see https://static.wikia.nocookie.net/tetrisconcept/images/3/3d/SRS-pieces.png/revision/latest/scale-to-width-down/336?cb=20060626173148
     const barPiece = [
         [GRID_WIDTH + 0, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3],
         [GRID_WIDTH * 2 + 0, GRID_WIDTH * 2 + 1, GRID_WIDTH * 2 + 2, GRID_WIDTH * 2 + 3],
         [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 3 + 1],
         [2, GRID_WIDTH + 2, GRID_WIDTH * 2 + 2, GRID_WIDTH * 3 + 2],
-    ]
+    ];
 
     const blueLPiece = [
         [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2],
         [1, 2, GRID_WIDTH + 1, GRID_WIDTH*2 + 1],
         [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 2],
         [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH],
-    ]
+    ];
 
     const orangeLPiece = [
         [2, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2],
         [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 2 + 2],
         [GRID_WIDTH * 2, GRID_WIDTH * 1, GRID_WIDTH * 1 + 1, GRID_WIDTH * 1 + 2],
         [0, 1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
-    ]
+    ];
 
     const boxPiece = [
         [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
         [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
         [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
         [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
-    ]
+    ];
 
     const greenZPiece = [
         [GRID_WIDTH, GRID_WIDTH + 1, 1, 2],
         [1, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 2],
         [GRID_WIDTH * 2, GRID_WIDTH * 2 + 1, GRID_WIDTH + 1, GRID_WIDTH + 2],
         [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
-    ]
+    ];
 
     const redZPiece = [
         [0, 1, GRID_WIDTH + 1, GRID_WIDTH + 2],
         [GRID_WIDTH * 2 + 1, GRID_WIDTH + 1, GRID_WIDTH + 2, 2],
         [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 2 + 2],
         [GRID_WIDTH * 2, GRID_WIDTH, GRID_WIDTH + 1, 1],
-    ]
+    ];
 
     const tPiece = [
         [GRID_WIDTH, GRID_WIDTH + 1, 1, GRID_WIDTH + 2],
         [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH + 2],
         [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH + 2],
         [GRID_WIDTH, GRID_WIDTH + 1, 1, GRID_WIDTH * 2 + 1]
-    ]
+    ];
 
-    const pieces = [barPiece, blueLPiece, orangeLPiece, boxPiece, greenZPiece, redZPiece, tPiece]
+    const pieces = [barPiece, blueLPiece, orangeLPiece, boxPiece, greenZPiece, redZPiece, tPiece];
 
-    let currentPosition = 4 // ?
-    let current = pieces[1][0]
+    let currentPosition = 3; // position of piece
 
-    // draw first rotation in first tetromino
+    // pick first piece randomly
+    let currentRotation = 0; // 0 - 3 rotations
+    let randPiece = Math.floor(Math.random() * pieces.length); // picks random 0-5
+    let currentPiece = pieces[randPiece][currentRotation];
+
+    // draw the piece by giving each of the 4 boxes that the piece occupies the "piece" class, which
+    // gives them the appropriate styling to indicate that it is a tetris piece
     function drawPiece() {
-        current.forEach(i => {
+        currentPiece.forEach(i => {
             squares[currentPosition + i].classList.add('piece') // add piece styling
         });
     }
-    drawPiece()
+    drawPiece();
+
+
+    function erasePiece() {
+        currentPiece.forEach(i => {
+            squares[currentPosition + i].classList.remove('piece') // add piece styling
+        });
+    }
 }) // this fires when index.html has been completely loaded 
 
